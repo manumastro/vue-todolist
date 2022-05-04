ed682cb38f2776885b59cb06f00fbd245e4aea83
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-05-04 16:39:57 +0200

    MILESTONE 1

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+const app = new Vue({
+  el: '#app',
+
+  data: {
+    toDoList: 
+    [
+
+      {
+        text: 'porta a spasso il cane',
+        done: false,
+      },
+      {
+        text: 'vai a fare la spesa',
+        done: true,
+      },
+      {
+        text: 'vai al meccanico',
+        done: false,
+      }
+    ]
+
+
+
+  }
+})
